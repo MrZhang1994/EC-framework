892a2f99397340d355300a75256f7266230f9787
Ren Wang <iamwrm@gmail.com>
2019-04-12 15:31:26 +0800

    linear total success

--- wr/new_test_bench/push.sh
+++ wr/new_test_bench/push.sh
@@ -1,13 +1,16 @@
 rm -f verification/w*/*.log
 rm -f verification/w*/local.file
+rm -f verification/w*/*.output
 
 rm -f verification/w2/*.zip
 rm -f verification/w2/*.txt
 rm -f verification/w2/*.output
 rm -rf verification/w2/test_images
+rm -rf verification/w2/images_to_send
 
 rm -f verification/w3/*.txt
 rm -rf verification/w3/test_images
+rm -rf verification/w3/images_to_send
 
 rm -rf verification/w*/__*
 rm -rf verification/w*/.keras
@@ -17,4 +20,4 @@ cp verification/shared_lib.py verification/w2/shared_lib.py
 cp verification/shared_lib.py verification/w3/shared_lib.py
 
 
-rsync -azvh . wr@10.211.55.93:/home/wr/py_ipc --delete
+rsync -azvh . wr@10.211.55.94:/home/wr/py_ipc --delete
